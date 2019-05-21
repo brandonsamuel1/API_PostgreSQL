e7d9d157ef19b85650b5fe08dbd2298508432dc1
Brandon <brandonsamuel72@gmail.com>
2019-05-21 14:39:33 -0700

    create postgesql db and connect it with sequelize

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const sequelize = new Sequelize(process.env.COURSES_API_DB, process.env.COURSES_API_USERNAME, process.env.COURSES_API_PASSWORD, {
+    host: process.env.COURSES_API_HOST,
+    dialect: 'postgres'
+});
+  
 
 app.get('/', (req, res) => {
-    res.send('Server connected');
+    res.send('Server and DB connected');
 });
 
 app.listen(8080, (req, res) => {
