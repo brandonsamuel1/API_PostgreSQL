bbf22d9fac06e2aa2d5c24f4ac93135d10211710
Brandon <brandonsamuel72@gmail.com>
2019-05-22 15:59:02 -0700

    get all courses

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv');
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
@@ -7,14 +7,41 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// CREATE DATABASE
 const sequelize = new Sequelize(process.env.COURSES_API_DB, process.env.COURSES_API_USERNAME, process.env.COURSES_API_PASSWORD, {
     host: process.env.COURSES_API_HOST,
     dialect: 'postgres'
 });
+
+const Courses = sequelize.define('course', {
+    title: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    }
+  });
   
+  // TEST DB CONNECTION
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
 
 app.get('/', (req, res) => {
     res.send('Server and DB connected');
+});
+
+// GET ALL COURSES
+app.get('/api/courses', (req, res) => {
+    sequelize.query('SELECT * FROM course').then((courses) => {
+        res.send(courses);
+    });
 });
 
 app.listen(8080, (req, res) => {
